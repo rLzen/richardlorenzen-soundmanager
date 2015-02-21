f2d393f35e707402fa8c1877480ef8dbc4959900
rcLzen <lorenzenr2@gator.uhd.edu>
2015-02-21 16:28:28 -0600

    changes on gemfile

--- rl_soundmanager/config/routes.rb
+++ rl_soundmanager/config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :microposts
   
   resources :users, :only => [:new, :show, :edit, :update, :destroy]
-  root 'application#hello'
+  root 'users#new'
+  #root 'application#hello'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
