591d869327f916767fef09dda0deda42eb638b70
rcLzen <lorenzenr2@gator.uhd.edu>
2015-02-21 16:51:21 -0600

    changes on gemfile

--- rl_soundmanager/config/routes.rb
+++ rl_soundmanager/config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   
   resources :users
   
-  root "application#users"
-  #root 'application#hello'
+  
+  root 'application#hello'
 end
 
   # The priority is based upon order of creation: first created -> highest priority.
