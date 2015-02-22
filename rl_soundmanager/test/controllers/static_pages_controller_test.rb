eafe4527fc9517d74eccb62fefe292c51b1c8a0b
rcLzen <lorenzenr2@gator.uhd.edu>
2015-02-22 01:47:36 -0600

    added custom helper full_title

--- rl_soundmanager/test/controllers/static_pages_controller_test.rb
+++ rl_soundmanager/test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Sound Manager Testing App"
+    assert_select "title", "Sound Manager Testing App"
   end
 
   test "should get help" do
