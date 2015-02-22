6c525545b5e3aedd7e3a18a96ba2fa379f5342c0
rcLzen <lorenzenr2@gator.uhd.edu>
2015-02-22 12:29:57 -0600

    added bootstrap to gemfile, added custom css

--- rl_soundmanager/app/helpers/application_helper.rb
+++ rl_soundmanager/app/helpers/application_helper.rb
@@ -1,11 +1,12 @@
 module ApplicationHelper
+  # Notes for understanding methods in ruby
   # Returns the full title on a per-page basis
   def full_title(page_title = '')
     base_title = "Sound Manager Testing App"
-    if page_title.empty?
+    if page_title.empty?                    # ? Boolean test
       base_title
     else
-      "#{page_title} | #{base_title}"
+      "#{page_title} | #{base_title}"       # String interpolation
     end
   end
 end
