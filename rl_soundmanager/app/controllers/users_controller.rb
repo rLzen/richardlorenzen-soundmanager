955d67084995f8c6dfb335741b02513c6e9c2e3f
rcLzen <lorenzenr2@gator.uhd.edu>
2015-02-22 21:46:30 -0600

    Finished with user signup

--- rl_soundmanager/app/controllers/users_controller.rb
+++ rl_soundmanager/app/controllers/users_controller.rb
@@ -1,7 +1,27 @@
 class UsersController < ApplicationController
  
-  def new
-    #@user = User.new
+  def show
+    @user = User.find(params[:id])
   end
-
+ 
+  def new
+    @user = User.new
+  end
+  # checking for a successful save
+  def create 
+    @user = User.new(user_params)
+    if @user.save
+      #flash success message
+      flash[:success] = 'Welcome to Sound Manager App!'
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  #auxiliary method
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end 
 end
--- rl_soundmanager/app/helpers/users_helper.rb
+++ rl_soundmanager/app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  # this will return the Gravatar for the given user (Gravatar is an image)
+  def gravatar_for(user)
+    # hexdigest to ensure that the argument is all lower-case
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, clas: "gravatar")
+  end
 end
