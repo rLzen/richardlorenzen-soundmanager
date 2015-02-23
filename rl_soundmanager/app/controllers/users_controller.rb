2fff50cd6a06df53a4adf3ea4374037fe5b42c96
rcLzen <lorenzenr2@gator.uhd.edu>
2015-02-23 14:14:40 -0600

    Finished with log in / log out

--- rl_soundmanager/app/controllers/users_controller.rb
+++ rl_soundmanager/app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create 
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       #flash success message
       flash[:success] = 'Welcome to Sound Manager App!'
       redirect_to @user
